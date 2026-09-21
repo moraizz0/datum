2dbac1a61210861601ffd6e23bf38e461b7c833f
Daniel Morais <daniel.moraizzz0@gmail.com>
2026-09-21 20:08:48 -0300

    first commit

---
+++ src/test/java/com/senai/datum/DatumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.datum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
